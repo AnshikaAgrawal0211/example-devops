86bfe13d62554be084686696e0430b39f42f1338
Anshika Agrawal <anshika.agrawal@knoldus.in>
2019-10-22 16:38:23 +0530

    added case to hit without postman

--- src/main/scala/app/devopsApp.scala
+++ src/main/scala/app/devopsApp.scala
@@ -2,6 +2,7 @@ package app
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
 import akka.stream.ActorMaterializer
 import controller.Controller
 
@@ -13,12 +14,20 @@ object devopsApp extends App {
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   implicit val materialize: ActorMaterializer = ActorMaterializer()
   private val routes = (new Controller).routes
+  private val listOfNames = List("anshika","abhishek","amit","ravi")
   private val serverBinding =  Http().bindAndHandle(
-      routes, "localhost", 9000)
+      routes, "localhost", 8000)
   serverBinding.onComplete {
     case Success(bound) =>
       println(
         s"Server online at http://${ bound.localAddress.getHostName }:${ bound.localAddress.getPort }/")
+      listOfNames.foreach{ name =>
+        val url = s"http://${ bound.localAddress.getHostName }:${ bound.localAddress.getPort }/welcome/${name}"
+        Http().singleRequest(HttpRequest(uri = url))
+      }
+      Http()(system)
+        .shutdownAllConnectionPools()
+      system.terminate()
     case Failure(error) =>
       Http()(system)
         .shutdownAllConnectionPools()
--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -7,6 +7,7 @@ class Controller {
 val routes:Route =
   get {
     pathPrefix("welcome" / Segment) { name =>
+      println(s"welcome $name \n")
       complete(
         s"welcome $name \n")
     }
